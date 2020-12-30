537912e6c0101d316c068d8eb992e7e4c12c5088
romanisthere <romanisthere@gmail.com>
2020-12-30 14:12:45 +0300

    add context

--- background/background.js
+++ background/background.js
@@ -2,12 +2,30 @@ chrome.runtime.onInstalled.addListener(async (details) => {
 	const { previousVersion, reason } = details
 
     if (reason == 'install') {
-        
+		chrome.storage.sync.get(['na_1'], resp => {
+            if (!resp.it_1) {
+                chrome.storage.sync.set({
+					na_1: ''
+                })
+            }
+        })
     } else if (reason == 'update') {
-		if (previousVersion === '0.1.16') {
+		if (previousVersion === '0.0.1') {
 
 		} else {
 
 		}
     }
+})
+
+chrome.contextMenus.removeAll()
+chrome.contextMenus.create({
+	title: 'MarkALink',
+	contexts: ['link'],
+	documentUrlPatterns: ["http://*/*", "https://*/*", "http://*/", "https://*/"],
+	onclick: (obj) => {
+		const linkUrl = obj.linkUrl
+		// console.log(obj)
+		console.log(linkUrl)
+	}
 })
