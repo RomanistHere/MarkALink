15790146200462f39d1370778964ba95d755562b
romanisthere <romanisthere@gmail.com>
2021-01-14 11:47:10 +0300

    refactor opt page

--- options/options.js
+++ options/options.js
@@ -3,39 +3,73 @@ import {
 	syncStore
 } from '../modules/helpers.js'
 
-const linkTemplate = (href) =>
-    `<a class="link" title="Open link in the new tab" target="_blank" href="${href}">
-        ${href}
-    </a>`
+const sectTempl = (grpName) =>
+	`<section class="${grpName} section">
+		<h2 class="title">
+			${grpName}
+		</h2>
+		<ul class="list">
+
+		</ul>
+	</section>`
+
+const markTempl = (url, mark) =>
+    `<li class="list__item">
+		<a href="${url}" class="list__link">${url}</a>
+		<p class="list__text">${mark}</p>
+	</li>`
+
+const sortByGrps = async (data) => {
+    let obj = {}
+    let curMax = 0
+    let mostGrp = ''
+
+    for (const prop in data) {
+        const grp = data[prop]['grp']
+
+        if (grp in obj) {
+            obj[grp] = [...obj[grp], data[prop]]
+        } else {
+            obj[grp] = [data[prop]]
+        }
+    }
+
+    return obj
+}
 
 const prepareItems = (arr = []) => {
 	const arrOfLinks = arr.map(href =>
-		linkTemplate(href))
+		markTempl(href))
 
 	return arrOfLinks.join('')
 }
 
 const init = async () => {
 	const data = await getData()
-	const keys = Object.keys(data)
-	const length = keys.length
+	const groups = sortByGrps(data)
+	const grpNames = Object.keys(groups)
+	let dataToRender = []
 
-	let marked = []
-	let hidden = []
+	for (let i = 0; i < grpNames.length; i++) {
 
-	for (let i = 0; i < length; i++) {
-		const key = keys[i]
-		if (data[key] === 'mark')
-			marked = [...marked, key]
-		else if (data[key] === 'hide')
-			hidden = [...hidden, key]
 	}
 
-	const markedLinks = prepareItems(marked)
-	document.querySelector('.marked').innerHTML = markedLinks
-
-	const hiddenLinks = prepareItems(hidden)
-	document.querySelector('.hidden').innerHTML = hiddenLinks
+	// let marked = []
+	// let hidden = []
+	//
+	// for (let i = 0; i < length; i++) {
+	// 	const key = keys[i]
+	// 	if (data[key] === 'mark')
+	// 		marked = [...marked, key]
+	// 	else if (data[key] === 'hide')
+	// 		hidden = [...hidden, key]
+	// }
+	//
+	// const markedLinks = prepareItems(marked)
+	// document.querySelector('.marked').innerHTML = markedLinks
+	//
+	// const hiddenLinks = prepareItems(hidden)
+	// document.querySelector('.hidden').innerHTML = hiddenLinks
 }
 
 init()
