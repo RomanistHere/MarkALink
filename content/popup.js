5f0c20f7327fda8f650a40c85c9f080bad046f00
romanisthere <romanisthere@gmail.com>
2021-01-19 06:41:39 +0300

    separate popup, add animation to content

--- content/popup.js
+++ content/popup.js
@@ -1,6 +1,49 @@
 const getPopupHTML = (linkUrl, groups, defGroup = 'Add new group', textArea = '', isMark = true) =>
     `<div class="MarkALink_popup__wrap">
-        <div class="MarkALink_popup__logo"></div>
+        <div class="MarkALink_popup__logo_wrap">
+            <div class="MarkALink_popup__logo">
+                <section class="logo_wrap">
+                    <div class="logo logo-based">
+                        <span class="logo__neon">Mark</span>
+                        <span class="logo__neon logo__neon-bot">ALink</span>
+                    </div>
+                    <div class="logo logo-shift">
+                        <span class="logo__neon">Mark</span>
+                        <span class="logo__neon logo__neon-bot">ALink</span>
+                    </div>
+                    <div class="logo logo-shift logo-shift-1">
+                        <span class="logo__neon">Mark</span>
+                        <span class="logo__neon logo__neon-bot">ALink</span>
+                    </div>
+                    <div class="logo logo-shift logo-shift-2">
+                        <span class="logo__neon">Mark</span>
+                        <span class="logo__neon logo__neon-bot">ALink</span>
+                    </div>
+                    <div class="logo logo-shift logo-shift-3">
+                        <span class="logo__neon">Mark</span>
+                        <span class="logo__neon logo__neon-bot">ALink</span>
+                    </div>
+                    <div class="logo logo-glitch">
+                        <div class="logo__neon">
+                            M<span class="logo-invis">a</span>rk
+                        </div>
+                        <div class="logo__neon logo__neon-bot">
+                            <span class="logo-invis">A</span>Link
+                            <span class="logo-trick">A</span>
+                        </div>
+                    </div>
+                    <div class="logo logo-glitch-shift">
+                        <div class="logo__neon">
+                            M<span class="logo-invis">a</span>rk
+                        </div>
+                        <div class="logo__neon logo__neon-bot">
+                            <span class="logo-invis">A</span>Link
+                            <span class="logo-trick">A</span>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
         <div class="MarkALink_popup__grp">
             <span class="MarkALink_popup__label">Url: </span>
             <input value=${linkUrl} type="text" class="MarkALink_popup__inp">
