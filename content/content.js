85782cf473cacdb201ef6b8fa02a03e6075e3205
romanisthere <romanisthere@gmail.com>
2021-01-20 14:03:00 +0300

    fix tooltip

--- content/content.js
+++ content/content.js
@@ -1,7 +1,55 @@
+const removeAll = () => {
+    domObserver.disconnect()
+    document.querySelectorAll('.MarkALink__marked').forEach(item => {
+        item.classList.remove('MarkALink__marked')
+        item.classList.remove('MarkALink__tooltiped')
+        item.removeAttribute("style")
+        item.removeEventListener('mouseenter', showTooltip)
+        item.removeEventListener('mousemove', moveTooltip)
+        item.removeEventListener('mouseleave', hideTooltip)
+    })
+    document.querySelectorAll('.MarkALink__blocked').forEach(item => {
+        item.classList.remove('MarkALink__blocked')
+        item.classList.remove('MarkALink__tooltiped')
+        item.removeEventListener('mouseenter', showTooltip)
+        item.removeEventListener('mousemove', moveTooltip)
+        item.removeEventListener('mouseleave', hideTooltip)
+    })
+    document.querySelectorAll('.MarkALink__tooltiped').forEach(item => {
+        item.classList.remove('MarkALink__tooltiped')
+        item.removeEventListener('mouseenter', showTooltip)
+        item.removeEventListener('mousemove', moveTooltip)
+        item.removeEventListener('mouseleave', hideTooltip)
+    })
+    document.querySelectorAll('.MarkALink__tooltip').forEach(item => item.remove())
+}
+
+const fillData = (tooltip, tooltipData) => {
+    const data = tooltipData.dataset
+    const grp = data.markalink_grp
+    const mark = data.markalink_mark
+
+    tooltip.querySelector('.MarkALink__tooltip_text-grp').textContent = grp
+    tooltip.querySelector('.MarkALink__tooltip_text-mark').textContent = mark
+
+    tooltip.classList.add('MarkALink__tooltip-show')
+}
+
 const showTooltip = (e) => {
-    const tooltip = e.target.classList.contains('MarkALink__tooltip')
+    const tooltipData = e.target.classList.contains('MarkALink__tooltip-hidden')
         ? e.target
-        : e.target.querySelector(':scope .MarkALink__tooltip')
+        : e.target.querySelector(':scope .MarkALink__tooltip-hidden')
+
+    const tooltip = document.querySelector('.MarkALink__tooltip')
+
+    if (!tooltipData || !tooltip)
+        return
+
+    fillData(tooltip, tooltipData)
+}
+
+const moveTooltip = (e) => {
+    const tooltip = document.querySelector('.MarkALink__tooltip')
 
     if (!tooltip)
         return
@@ -14,33 +62,31 @@ const showTooltip = (e) => {
         : (document.body.clientHeight + 5 - tooltip.clientHeight + 'px')
 }
 
-const removeAll = () => {
-    domObserver.disconnect()
-    document.querySelectorAll('.MarkALink__marked').forEach(item => {
-        item.classList.remove('MarkALink__marked')
-        item.classList.remove('MarkALink__tooltiped')
-        item.removeAttribute("style")
-        item.removeEventListener('mousemove', showTooltip)
-    })
-    document.querySelectorAll('.MarkALink__blocked').forEach(item => {
-        item.classList.remove('MarkALink__blocked')
-        item.classList.remove('MarkALink__tooltiped')
-        item.removeEventListener('mousemove', showTooltip)
-    })
-    document.querySelectorAll('.MarkALink__tooltiped').forEach(item => {
-        item.classList.remove('MarkALink__tooltiped')
-        item.removeEventListener('mousemove', showTooltip)
-    })
-    document.querySelectorAll('.MarkALink__tooltip').forEach(item => item.remove())
+const hideTooltip = (e) => {
+    const tooltip = document.querySelector('.MarkALink__tooltip')
+
+    tooltip.querySelector('.MarkALink__tooltip_text-grp').textContent = null
+    tooltip.querySelector('.MarkALink__tooltip_text-mark').textContent = null
+
+    tooltip.classList.remove('MarkALink__tooltip-show')
 }
 
-const getTooltip = ({ grp, mark, type }) =>
+const getTooltipHdn = ({ grp, mark, type }) =>
+    `<span data-MarkALink_grp="${grp}" data-MarkALink_mark="${mark}"" class="MarkALink__tooltip-hidden"></span>`
+
+const getTooltip = () =>
     `<div data-PopUpOFF="notification" class="MarkALink__tooltip">
-        <span class="MarkALink__tooltip_text MarkALink__tooltip_text-grp">${grp}</span>
-        <span class="MarkALink__tooltip_text MarkALink__tooltip_text-mark">${mark}</span>
+        <span class="MarkALink__tooltip_text MarkALink__tooltip_text-grp">grp</span>
+        <span class="MarkALink__tooltip_text MarkALink__tooltip_text-mark">mark</span>
     </div>`
 
+const initSideElements = () => {
+    const tooltipHTML = getTooltip()
+    document.body.insertAdjacentHTML('beforeend', tooltipHTML)
+}
+
 const loopThorugh = (links, data, customSettings, pairs) => {
+    initSideElements()
     // console.log(data)
     links.forEach(item => {
         const url = item.href
@@ -49,10 +95,12 @@ const loopThorugh = (links, data, customSettings, pairs) => {
 
             // tooltip
             if (!item.classList.contains('MarkALink__tooltiped')) {
-                const tooltipHTML = getTooltip(data[url])
+                const tooltipHTML = getTooltipHdn(data[url])
                 item.insertAdjacentHTML('beforeend', tooltipHTML)
                 item.classList.add('MarkALink__tooltiped')
-                item.addEventListener('mousemove', showTooltip)
+                item.addEventListener('mouseenter', showTooltip)
+                item.addEventListener('mousemove', moveTooltip)
+                item.addEventListener('mouseleave', hideTooltip)
             }
 
             const grpName = data[url].grp
